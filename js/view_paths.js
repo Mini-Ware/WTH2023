09c605c1ce7c24478894dff741d3ea2a2981d3fc
Mini-Ware <codauknow@gmail.com>
2023-08-26 21:03:07 +0800

    get next instruction in view path

--- js/view_paths.js
+++ js/view_paths.js
@@ -4,7 +4,13 @@ function play_path(id, ip){
     document.getElementById("ip").innerText = "Instruction: "+ip_data["direction"]+", "+ip_data["steps"]+" steps ahead";
 }
 
-function play_next(){}
+function play_next(){
+    if (ip < JSON.parse(localStorage.getItem(id)).length-1){
+        window.open("view_paths.html?path="+id+"&id="+Number(ip+1).toString(), "_self")
+    }else{
+        window.open("/","_self")
+    }
+}
 
 let params = new URLSearchParams(document.location.search);
 id = params.get("path");
