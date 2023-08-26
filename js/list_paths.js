eb121aa20ef96ec3c8567d2a4bda89dfb9cdb7e0
Mini-Ware <codauknow@gmail.com>
2023-08-26 21:16:25 +0800

    added no path message when list with no path

--- js/list_paths.js
+++ js/list_paths.js
@@ -1,28 +1,24 @@
 function create_new_fields(){
 
-    for (i=0;i<localStorage.length;i++){
-        console.log(i)
-        initial_field = document.getElementById("path_list_copy").children[0].cloneNode(true);
-        
-        const path_data = localStorage.key(i);
-
-        initial_field.querySelector("h2").innerText = "Name of path: "+path_data;
-        initial_field.querySelector("p").innerText = "Instructions ("+JSON.parse(localStorage.getItem(localStorage.key(i))).length+"): left, right";
-
-        initial_field.addEventListener("click", ()=>{window.open("view_paths.html?path="+path_data+"&id=0", "_self")});
-
-        document.getElementById("path_list").appendChild(initial_field)
+    if (localStorage.length != 0){
+        for (i=0;i<localStorage.length;i++){
+            console.log(i)
+            initial_field = document.getElementById("path_list_copy").children[0].cloneNode(true);
+            
+            const path_data = localStorage.key(i);
+    
+            initial_field.querySelector("h2").innerText = "Name of path: "+path_data;
+            initial_field.querySelector("p").innerText = "Instructions ("+JSON.parse(localStorage.getItem(localStorage.key(i))).length+"): left, right";
+    
+            initial_field.addEventListener("click", ()=>{window.open("view_paths.html?path="+path_data+"&id=0", "_self")});
+    
+            document.getElementById("path_list").appendChild(initial_field)
+        }
+    }else{
+        no_path_err = document.createElement("h1");
+        no_path_err.innerText = "No paths available! Go back and create a new path!";
+        document.getElementById("path_list").appendChild(no_path_err);
     }
 }
 
 create_new_fields();
-
-function retrieve_paths(){
-
-}
-
-function display_paths(paths_info){
-    
-}
-
-display_paths(retrieve_paths())
