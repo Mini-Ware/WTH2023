2bbae75e91e05af305b1ddb38a0d94854e1b883a
Mini-Ware <codauknow@gmail.com>
2023-08-26 20:41:26 +0800

    added list paths

--- js/create_paths.js
+++ js/create_paths.js
@@ -1,7 +1,10 @@
-const initial_field = document.getElementById("item_list").innerHTML;
-
 function create_new_field(){
-    document.getElementById("item_list").innerHTML += initial_field;
+    initial_field = document.getElementById("item_list").children[0].cloneNode(true);
+
+    initial_field.querySelector(".direction_data").value = "";
+    initial_field.querySelector(".steps_data").value = "";
+    
+    document.getElementById("item_list").appendChild(initial_field)
 }
 
 path_data_example = [{
@@ -10,33 +13,25 @@ path_data_example = [{
 }]
 
 function gather_data_from_form(){
-    path_data = {
-        name: document.getElementById("path_name_data").value,
-        instructions: []
-    }
+    path_data = []
     form = document.getElementById("item_list").children;
     for (i=0;i<form.length;i++){
-        direction = form[i].querySelector("#direction_data").value;
-        steps = form[i].querySelector("#steps_data").value;
-        path_data.instructions.push({
-                direction: direction,
-                steps: steps
+        direction = form[i].querySelector(".direction_data").value;
+        steps = form[i].querySelector(".steps_data").value;
+        path_data.push({
+            direction: direction,
+            steps: steps
         })
     }
     return path_data;
 }
 
 function store_path(path){
-    if (localStorage.paths){
-        current = localStorage.getItem("paths");
-        localStorage.setItem("paths", current.append(path));
-    }else{
-        localStorage.setItem("paths", [path]);
-    }
+    localStorage.setItem(document.getElementById("path_name_data").value, JSON.stringify(path));
 }
 
 function submit_new_path(){
     console.log(gather_data_from_form());
-//    store_path(gather_data_from_form());
+    store_path(gather_data_from_form());
     window.open("list_paths.html", "_self")
 }
