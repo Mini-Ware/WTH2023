953483f7aed6dc1d8c7788c01add6570053b6033
Mini-Ware <codauknow@gmail.com>
2023-08-27 09:00:05 +0800

    text to speech

--- js/view_paths.js
+++ js/view_paths.js
@@ -2,6 +2,7 @@ function play_path(id, ip){
     ip_data = JSON.parse(localStorage.getItem(id))[Number(ip)];
     document.getElementById("path_name").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">    <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z" />  </svg> Path Name : '+id;
     document.getElementById("ip").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">    <path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />  </svg>  Instruction: '+ip_data["direction"]+", "+ip_data["steps"]+" steps ahead";
+    tts(ip_data["direction"]+", "+ip_data["steps"]+" steps ahead");
 }
 
 function play_next(){
@@ -19,4 +20,12 @@ play_path(id, ip);
 
 if (ip == JSON.parse(localStorage.getItem(id)).length-1){
     document.getElementById("play_button").innerText="END";
+}
+
+
+// text-to-speech
+function tts(text){
+    const synth = window.speechSynthesis;
+    const utterThis = new SpeechSynthesisUtterance(text);
+    synth.speak(utterThis);
 }
